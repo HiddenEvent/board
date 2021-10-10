c1f442d39a7bc36f57d0a8e0f66e0ad70c90c9d6
RichardKim <hiddeneventor@gmail.com>
2021-10-10 23:29:38 +0900

    AOP 관련 추가중

--- README.md
+++ README.md
@@ -19,4 +19,10 @@ springBoot 개념정리
 implementation 'org.springframework.boot:spring-boot-starter-aop'
 ```
 2. 어노테이션 관련
+   - @Before : 메소드 호출되기 전 실행
+   - @After : 메소드 호출된 후 실행
+   - @AfterReturning:  메소드가 성공적으로 실행된 이후에 실행해라
+   - @AfterThrowing : 메소드 실행중에 예외가 던져지면 수행해라
+   - @Around : 메소드 수행 전후에 실행해라
+   
    
--- src/main/java/com/richard/board/aspect/LoggingAspect.java
+++ src/main/java/com/richard/board/aspect/LoggingAspect.java
@@ -1,16 +1,38 @@
 package com.richard.board.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component /* Aspect가 서버에 올라갈때 같이 올라간다.*/
 public class LoggingAspect {
-    /*
-    *  @Before
-    *  -
-    *
-    *
-    * */
+    /*execution() 은 페키지를 조햅해서 적는 란*/
+    @Before("execution(* com.richard.board.service.*.get*(..))") /* get으로 시작되는 모든 메서드 */
+    public void loggerBefore() {
+        System.out.println("get 으로 시작되는 메서드 시작");
+    }
+
+    @After("execution(* com.richard.board.service.*.get*(..))") /* get으로 시작되는 모든 메서드 */
+    public void loggerAfter() {
+        System.out.println("get 으로 시작되는 메서드 끝");
+    }
+
+//    public Object loggerAround(ProceedingJoinPoint pjp) throws Throwable {
+//        long beforeTimeMillis = System.currentTimeMillis();
+//        System.out.println("[UserController] 실행시작 : "
+//                + pjp.getSignature().getDeclaringTypeName() + "."
+//                + pjp.getSignature().getName()
+//        );
+//        Object result = pjp.proceed();
+//
+//        long afterTimeMillis = System.currentTimeMillis() - beforeTimeMillis;
+//        System.out.println(
+//                "[UserController] 시"
+//
+//        );
+//    }
 
 }
